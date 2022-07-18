7faffed3537ac5e3a6539561057a2d9813361c33
Emma <jihyelee.329@gmail.com>
2022-07-18 16:10:50 +0900

    add : Class Component React-movie-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './routes/Home'
-import Detail from './routes/Detail'
-import './App.css'
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
-function App() {
-  return (<Router>  
-    {/* Router 에서는 두개의 Route를 한번에 렌더링 가능
-      Router은 동적 url을 지원함. 동적 url은 변수를 쓸수있다는 뜻 
-      Switch 역할 : Route 찾고, 한 번에 하나의 Route만 렌더링
-      BrowserRouter과 Router 차이점은 URL의 모습 ( 브라우저라우터는 우리가 아는 url모양) 
-    */}
-      {/* path경로 에 :id 부분은 movie페이지에서 id란 변수로 url을 받을거다~란 뜻 */}
-      <Routes>
-
-        <Route path="/movie/:id"element={<Detail/>} />
-        <Route path="/" element={<Home/>}/>   {/* Route 역할 : Route 안에 컴포넌트 써주기  */}
-       
-      </Routes>
-  </Router>
-  );
+// class component는 class임. but, react component 로부터 확장되고 screen 에 표시됨
+// react 는 자동적으로 class component 의 render method를 실행한다.
+class App extends React.Component {
+  state = {
+    //state = object(객체). component 의 data를 넣을 공간이 있고 이 데이터는 변함
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    //async await 를 쓴다 = js에게 getMovies function애개 조금 시간이 필요하고,
+    // 우린 그걸 기다려야 한다는 뜻!!
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    console.log(movies);
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  genres={movie.genres}
+                  year={movie.year}
+                  poster={movie.medium_cover_image}
+                  title={movie.title}
+                  summary={movie.summary}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
